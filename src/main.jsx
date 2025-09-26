a151850442805ca5ba4d788efda2341886c096ef
Niko <niko@mac.speedport.ip>
2025-09-26 10:17:51 +0200

    Initial version of multi turn vis

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ConversationExplorer from './ConversationExplorer.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConversationExplorer />
+  </React.StrictMode>
+)
